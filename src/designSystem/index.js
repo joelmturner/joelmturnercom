9436188f02c9d98020ac31532f0bb50fcfa8b12e
Nathan Rothenberger <nroth10@gmail.com>
2018-07-19 23:06:46 -0700

    Stub out design system-y stuff

---
+++ src/designSystem/index.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+// Theme Placeholder
+export const colors = {
+    navy: '#1D3654',
+    gold: '#CA9874'
+}
+
+// Reusable Style Functions
+export const themeHover = () => css`
+  fill: ${colors.navy};
+  color: ${colors.navy};
+  
+  &:hover {
+    fill: ${colors.gold};
+    color: ${colors.gold};
+  }
+`
