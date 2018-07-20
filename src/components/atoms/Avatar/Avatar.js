3ec68ab27579593885985901562845f61fe94801
Nathan Rothenberger <nroth10@gmail.com>
2018-07-20 00:17:24 -0700

    Stub out Avatar

---
+++ src/components/atoms/Avatar/Avatar.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Avatar = styled.div`
+  width: 8rem;
+  height: 8rem;
+  border-radius: 50%;
+  background-size: cover;
+  background-color: pink;
+  background-image: url('https://placekitten.com/g/128');
+`;
+
+export default Avatar;
