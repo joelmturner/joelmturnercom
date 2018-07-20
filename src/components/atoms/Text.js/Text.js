20e2535b20bed245b945fb93c5c52b24be87cc93
Nathan Rothenberger <nroth10@gmail.com>
2018-07-20 00:17:59 -0700

    Update text components w/ Source sans. add index export.

--- src/components/atoms/Text.js/Text.js
+++ src/components/atoms/Text.js/Text.js
@@ -1,15 +1,24 @@
 import styled from 'styled-components';
+import {sharedFontStyles} from '../../../designSystem';
 
 // Headline
-const Text1 = styled.h1`
+export const Text1 = styled.h1`
+  ${sharedFontStyles()}
   font-size: 24px;
   font-weight: bold;
   line-height: 2rem;
   margin: 0;
 `;
 
-const Text2 = styled.h4`
+export const Text2 = styled.h4`
+  ${sharedFontStyles()}
   font-size: 0.75rem;
   line-height: 1.125rem;
   margin: 0;
 `
+
+// Body
+export const BodyText = styled.p`
+  ${sharedFontStyles()};
+`;
+
