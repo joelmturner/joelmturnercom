38879b87a257a69142680b0ba13c685e4ef84bf6
Nathan Rothenberger <nroth10@gmail.com>
2018-07-19 23:05:29 -0700

    Stub out some Text components

---
+++ src/components/atoms/Text.js/Text.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+// Headline
+const Text1 = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 2rem;
+  margin: 0;
+`;
+
+const Text2 = styled.h4`
+  font-size: 0.75rem;
+  line-height: 1.125rem;
+  margin: 0;
+`
