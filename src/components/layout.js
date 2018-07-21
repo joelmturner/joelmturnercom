6f63b58fdf6ee82cc63b0536a8396343f5951300
Joel <joel@joelmturner.com>
2018-07-20 22:57:27 -0700

    rolling back

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql, Link } from "gatsby"
-import Avatar from './Avatar/Avatar';
 import './layout.css'
 import {Box, Flex, Heading} from 'rebass';
 
@@ -31,7 +30,6 @@ const Layout = ({ children, data }) => (
                 is='h1'
                 fontSize={[ 4, 5, 6 ]}>
                 <Link style={{color: 'white'}} to={'/'}>{data.site.siteMetadata.title}</Link>
-                <Avatar url='' size='m' />
             </Heading>
         </Box>
         <Flex
