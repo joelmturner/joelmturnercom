fe39847170dea83d426f23d6d19c6fc812296d04
Nathan Rothenberger <nroth10@gmail.com>
2018-07-21 22:30:24 -0700

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,35 +24,9 @@ class IndexPage extends Component {
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
 
+        <hr />
+
         <h2>Pages</h2>
-        
-        {posts && posts[0] && (
-          
-          <PostEntryContainer>
-            <ImageSpacer>
-              <Image src={getNewImage()}/>
-              <BlogTitle>{posts[0].node.title}</BlogTitle>
-            </ImageSpacer>
-            <BlogContentWrap>
-              <BlogBlurb>
-                Listicle aesthetic mixtape umami kombucha schlitz farm-to-table, street art organic crucifix truffaut chambray deep v fam pork belly. Four loko chillwave hexagon organic, narwhal single-origin coffee everyday carry disrupt vaporware humblebrag. Tofu cred venmo, health goth live-edge cronut air plant tumblr locavore meggings quinoa edison bulb kinfolk kale chips single-origin coffee. Keffiyeh gentrify authentic, franzen blog letterpress 8-bit tilde. Kale chips kogi cardigan DIY, man braid swag actually tbh palo santo portland chia.
-              </BlogBlurb>
-              <TextLink>Read More</TextLink>
-            </BlogContentWrap>
-          </PostEntryContainer>
-        )}
-
-        <div style={{
-          background: 'red',
-          width: '100%',
-          paddingTop: '56.25%'
-          
-        }}>
-          hi
-        </div>
-        
-
-        
         {pages && pages.map(page => (
             <li key={page.node.id}>
               <Link to={`/${page.node.slug}`}>{page.node.title}</Link>
@@ -65,7 +39,9 @@ class IndexPage extends Component {
               <Link to={`/${post.node.slug}`}>{post.node.title}</Link>
             </li>
         ))}
+
         <hr/>
+        
         {posts && posts.map(post => {
           const {id, title, slug} = post.node;
           return (
