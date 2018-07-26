a07ee31291ca337553bd367c7b9503b9adca3535
Nathan Rothenberger <nroth10@gmail.com>
2018-07-25 23:37:14 -0700

    Section style updates

--- src/components/atoms/Section/Section.js
+++ src/components/atoms/Section/Section.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { bodyWrapperStyles } from '../../../designSystem';
 
 const Section = styled.div`
+  margin-bottom: 2.5rem;
   /* ${bodyWrapperStyles()}; */
 `;
 
