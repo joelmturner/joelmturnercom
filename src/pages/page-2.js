ea55c4b75d6cd8f3ad5b6ce7a4df5badabdcb942
Nathan Rothenberger <nroth10@gmail.com>
2018-07-26 23:48:21 -0700

    Add dummy text styleguide to page 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import { H1, H2, H3, H4, BodyText, LinkText } from '../components/atoms/Text';
 import Layout from '../components/layout'
 
 const SecondPage = props => {
@@ -12,6 +12,18 @@ const SecondPage = props => {
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2, where we list all the OTHER pages</p>
       
+      <h2>Type Styles</h2>
+      <H1>Header 1</H1>
+      <H1 gold>Header 1</H1>
+      <H2>Header 2</H2>
+      <H3>Header 3</H3>
+      <H4>Header 4</H4>
+      <BodyText>BodyText</BodyText>
+      <BodyText grey>BodyText Grey</BodyText>
+      <LinkText>Link Text</LinkText>
+      <div />
+      <LinkText jumbo>Link Text Jumbo</LinkText>
+
       <h2>Pages</h2>
       {pages && pages.map(page => (
         <li key={page.node.id}>
