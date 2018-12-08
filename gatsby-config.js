786b6b9ab67d52a79b7a60e14aae0c202f798c9b
Joel <joel@joelmturner.com>
2018-12-07 19:41:33 -0800

    add siteurl

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
     description: 'Dev, Design, and Kombucha',
+    siteUrl: 'https://joelmturner.com'
     about: {
       name: 'Joel M. Turner',
       title: 'Frontend Developer',
