a2b8d2442139e7d6cfc4b0baef62989e8d7a1aa5
Joel <joel@joelmturner.com>
2019-09-04 08:38:53 -0700

    move utils and queries to utils folder

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react"
-import { getImageStep } from "../utils"
+import { getImageStep } from "../utils/utils"
 import { GalleryImage } from "../components/gallery"
 
 type Lightbox = {
