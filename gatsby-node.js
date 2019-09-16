248a7284ecfc6f45eb07cddf3a2158840f987c4b
Joel <joel@joelmturner.com>
2019-09-15 19:48:09 -0700

    add category and ogImage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,47 @@
-exports.createSchemaCustomization = require("./scripts/createSchemaCustomization").createSchemaCustomization
-exports.createPages = require("./scripts/createPages").createPages
-exports.onCreateNode = require("./scripts/onCreateNode").onCreateNode
+// exports.createSchemaCustomization = require("./scripts/createSchemaCustomization").createSchemaCustomization
+// exports.createPages = require("./scripts/createPages").createPages
+// exports.onCreateNode = require("./scripts/onCreateNode").onCreateNode
+exports.onCreateNode = async ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `Mdx`) {
+    createNodeField({
+      node,
+      name: "category",
+      value: node.frontmatter.category,
+    })
+    createNodeField({
+      node,
+      name: "ogImage",
+      value: node.frontmatter.ogImage,
+    })
+  }
+}
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes } = actions
+  createTypes(`
+    type MdxBlogPost implements BlogPost {
+      frontmatter: MdxFrontmatter!
+    }
+    type MdxFrontmatter {
+      category: String
+      ogImage: String
+    }
+  `)
+
+  createTypes(
+    schema.buildObjectType({
+      name: `MdxBlogPost`,
+      fields: {
+        category: {
+          type: `String`,
+        },
+        ogImage: {
+          type: `String`,
+        },
+      },
+      interfaces: [`Node`, `BlogPost`],
+    })
+  )
+}
