b6ce595f747e6c5470d62fd8524559a977782c3d
Joel <joel@joelmturner.com>
2019-09-26 22:34:02 -0700

    add privacy policy

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Layout } from "../components"
+import Policy from "../utils/content-snippets/privacy-policy.mdx"
+
+export default () => (
+  <Layout>
+    <Policy />
+  </Layout>
+)
