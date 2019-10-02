98b274dc5ee0310488f0d5ffc9f0092c78f0dcc1
Joel <joel@joelmturner.com>
2019-10-01 22:04:29 -0700

    add social icons

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,9 @@
 import { jsx, Styled } from "theme-ui"
 import { ReactNode } from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { IoLogoInstagram, IoLogoGithub, IoLogoTwitter } from "react-icons/io"
 import Header from "./header"
+import { Flexbox } from "."
 
 type LayoutProps = {
   children: ReactNode | ReactNode[];
@@ -26,12 +28,26 @@ export default ({ children, title = "", className }: LayoutProps): JSX.Element =
       <Header siteTitle={title ? title : data.site.siteMetadata.title} />
       <main sx={{ variant: "content.wrapper" }}>{children}</main>
       <footer sx={{ mt: 2 }}>
-        <div sx={{ variant: "content.wrapper", borderTop: "1px solid", borderColor: "muted", py: 3 }}>
-          © 2012-{new Date().getFullYear()}, Proudly built with
-          {` `}
-          <Styled.a href="https://www.gatsbyjs.org">Gatsby</Styled.a> and{" "}
-          <Styled.a href="https://theme-ui.com">Theme UI</Styled.a>
-        </div>
+        <Flexbox middle sx={{ variant: "content.wrapper", borderTop: "1px solid", borderColor: "muted", py: 3 }}>
+          <Flexbox left gap={2}>
+            <Styled.p sx={{ fontSize: 2, mb: 0 }}>
+              © 2012-{new Date().getFullYear()}, Proudly built with{` `}
+              <Styled.a href="https://www.gatsbyjs.org">Gatsby</Styled.a> and{" "}
+              <Styled.a href="https://theme-ui.com">Theme UI</Styled.a>
+            </Styled.p>
+          </Flexbox>
+          <Flexbox gap={2} right>
+            <Styled.a href="https://www.instagram.com/joelmturner/" target="_blank" title="instagram">
+              <IoLogoInstagram />
+            </Styled.a>
+            <Styled.a href="https://twitter.com/joelmturner" target="_blank" title="twitter">
+              <IoLogoTwitter />
+            </Styled.a>
+            <Styled.a href="https://github.com/joelmturner" target="_blank" title="github">
+              <IoLogoGithub />
+            </Styled.a>
+          </Flexbox>
+        </Flexbox>
       </footer>
     </Styled.root>
   )
