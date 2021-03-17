83d72d37d6540b4bcff1049bf9449b4987a2fa48
Joel <joel@joelmturner.com>
2021-03-17 10:03:23 -0700

    feat(theme): switches to use HSL colors

--- src/hooks/useKeypress.ts
+++ src/hooks/useKeypress.ts
@@ -1,34 +1,37 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 export function useKeypress(targetKey: string) {
-    // State for keeping track of whether key is pressed
-    const [keyPressed, setKeyPressed] = useState(false)
-   
-    // Add event listeners
-    useEffect(() => {
-      // If pressed key is our target key then set to true
-      function downHandler({ key }: KeyboardEvent) {
-        if (key === targetKey) {
-          setKeyPressed(true)
-        }
-      }
-    
-      // If released key is our target key then set to false
-      const upHandler = ({ key }: KeyboardEvent) => {
-        if (key === targetKey) {
-          setKeyPressed(false)
-        }
-      }
-      window.addEventListener("keydown", downHandler)
-      window.addEventListener("keyup", upHandler)
-      // Remove event listeners on cleanup
-      return () => {
-        window.removeEventListener("keydown", downHandler)
-        window.removeEventListener("keyup", upHandler)
-      }
-    }, [targetKey]) // Empty array ensures that effect is only run on mount and unmount
-  
-    return keyPressed
-  }
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
 
-  export default useKeypress
+  // Add event listeners
+  useEffect(() => {
+    // If pressed key is our target key then set to true
+    function downHandler({ key }: KeyboardEvent) {
+      console.log("key", key);
+      console.log("targetKey", targetKey);
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    }
+
+    // If released key is our target key then set to false
+    const upHandler = ({ key }: KeyboardEvent) => {
+      if (key === targetKey) {
+        console.log("key", key);
+        setKeyPressed(false);
+      }
+    };
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, [targetKey]); // Empty array ensures that effect is only run on mount and unmount
+
+  return keyPressed;
+}
+
+export default useKeypress;
