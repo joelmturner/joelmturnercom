d8075140758179163db0274188d9ded4be99a6a9
Joel <joel@joelmturner.com>
2021-04-08 22:03:42 -0700

    chore(infra): turns off bundle analytics for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -238,6 +238,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-webpack-bundle-analyser-v2`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
   ],
 };
