d18210dd745acbdffd58533b421f3dfbf576857d
Joel <joel@joelmturner.com>
2021-04-26 22:39:17 -0700

    chore(rss): adds rss feed with excerpts

--- gatsby-config.js
+++ gatsby-config.js
@@ -222,59 +222,60 @@ module.exports = {
       },
     },
     // throwing error for some reason
-    // {
-    //   resolve: `gatsby-plugin-feed`,
-    //   options: {
-    //     query: `
-    //         {
-    //           site {
-    //             siteMetadata {
-    //               title
-    //               description
-    //               siteUrl
-    //               site_url: siteUrl
-    //             }
-    //           }
-    //         }
-    //       `,
-    //     feeds: [
-    //       {
-    //         serialize: ({ query: { site, allMdx } }) => {
-    //           return allMdx.nodes.map((node) => {
-    //             return Object.assign({}, node.frontmatter, {
-    //               description: node.excerpt,
-    //               date: node.frontmatter.date,
-    //               url: site.siteMetadata.siteUrl + node.slug,
-    //               guid: site.siteMetadata.siteUrl + node.slug,
-    //               custom_elements: [{ "content:encoded": node.html }],
-    //             });
-    //           });
-    //         },
-    //         query: `
-    //             {
-    //                 allMdx(
-    //                     sort: {order: DESC, fields: frontmatter___date}
-    //                     filter: { fileAbsolutePath: {regex: "/\/content/blog\/g"}}
-    //                   ) {
-    //                     nodes {
-    //                       excerpt
-    //                       frontmatter {
-    //                         title
-    //                         date
-    //                         category
-    //                       }
-    //                       slug
-    //                       html
-    //                     }
-    //                   }
-    //             }
-    //           `,
-    //         output: "/rss.xml",
-    //         title: "Your Site's RSS Feed",
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  site_url: siteUrl
+                }
+              }
+            }
+          `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.nodes.map((node) => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.slug,
+                  guid: site.siteMetadata.siteUrl + node.slug,
+                  //   custom_elements: [{ "content:encoded": node.html }],
+                });
+              });
+            },
+            query: `
+                {
+                    allMdx(
+                        sort: {order: DESC, fields: frontmatter___date}
+                        filter: { fileAbsolutePath: {regex: "/\/content/blog\/g"}}
+                      ) {
+                        nodes {
+                          excerpt
+                          frontmatter {
+                            title
+                            date
+                            category
+                          }
+                          slug
+                        }
+                      }
+                }
+              `,
+            output: "/rss.xml",
+            title: "joelmturner.com's RSS Feed",
+            site_url: "https://joelmturner.com",
+            match: "^/blog/",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
