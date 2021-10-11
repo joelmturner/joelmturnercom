09c38835f9a1a49d554667817a9fa08b80b46aa1
Joel <joel@joelmturner.com>
2021-10-11 01:30:41 -0700

    fix: comments out sitemap due to error

--- gatsby-config.js
+++ gatsby-config.js
@@ -144,87 +144,42 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       },
     },
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        output: `/sitemap.xml`,
-        // Exclude specific pages or groups of pages using glob parameters
-        // See: https://github.com/isaacs/minimatch
-        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-        excludes: [
-          `category/*`,
-          `tag/*`,
-          `til/null/*`,
-          `/til/illustration*`,
-          `/til/productivity*`,
-          "/til/lifestyle*",
-          `/til/dev*`,
-          `/til/null*`,
-          `/blog/uses/`,
-          `/blog/privacy-policy/`,
-          `/blog/storybook/`,
-          `/blog/vs-code/`,
-          `/blog/helpers/`,
-          "/blog/mobx/",
-          `/blog/react-typescript/`,
-          `/blog/data-vis/`,
-          `/blog/concepts/`,
-          `/blog/about/`,
-          `/blog/intro/`,
-          "/til/dev/react-layout-components/",
-          "/til/dev/sassy-with-sass/",
-          "/til/dev/react-hooks-useslider/",
-          "/til/dev/react-hooks-use-dims/",
-          "/til/dev/quick-tip-graphql-fragments/",
-          "/til/dev/productivity-practices-front-end-development/",
-          "/til/lifestyle/pmj-artwork/",
-          "/til/illustration/playing-with-type/",
-          "/til/dev/inline-text-edit-react-hooks/",
-          "/til/illustration/handlettering-best-of-2016/",
-          "/til/productivity/front-end-ticket-checklist/",
-          "/til/lifestyle/doctors-companions/",
-          "/til/personal-development/decisions-we-make/",
-          "/til/personal-development/daily-routine-freedom/",
-          "/til/dev/data-vis-react-bar-chart-vx/",
-          "/til/dev/create-a-single-line-chart-in-react-with-vx/",
-          "/til/dev/beginning-web-accessibility-react/",
-          "/til/dev/avatar-component-gatsby-3/",
-          "/til/dev/build-gallery-react-css-grid/",
-          "/til/dev/avatar-component-gatsby-2/",
-          "/til/dev/avatar-component-gatsby-1/",
-          "/til/dev/animated-css-timer-icon/",
-          "/til/dev/svelte-animated-water-svg-pictorial-fraction/",
-          "/til/dev/sequential-interval-react-hook",
-          "/til/personal-development/concepts/",
-          "til/null/",
-        ],
-        query: `
-        {
-          site {
-            siteMetadata {
-              siteUrl
-            }
-          }
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-      }`,
-        resolveSiteUrl: ({ site }) => {
-          //Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
-          return site.siteMetadata.siteUrl;
-        },
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.nodes.map((node) => {
-            return {
-              url: `${site.siteMetadata.siteUrl}${node.path}`,
-              changefreq: `daily`,
-              priority: 0.7,
-            };
-          }),
-      },
-    },
+    // throwing TypeError: Cannot read property 'nodes' of undefined
+    // {
+    //   resolve: `gatsby-plugin-sitemap`,
+    //   options: {
+    //     output: `/sitemap.xml`,
+    //     // Exclude specific pages or groups of pages using glob parameters
+    //     // See: https://github.com/isaacs/minimatch
+    //     // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+    //     excludes: [`category/*`, `tag/*`, `til/null/*`],
+    //     query: `
+    //     {
+    //       site {
+    //         siteMetadata {
+    //           siteUrl
+    //         }
+    //       }
+    //       allSitePage {
+    //         nodes {
+    //           path
+    //         }
+    //       }
+    //   }`,
+    //     resolveSiteUrl: ({ site }) => {
+    //       //Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+    //       return site.siteMetadata.siteUrl;
+    //     },
+    //     serialize: ({ site, allSitePage }) =>
+    //       allSitePage.nodes.map((node) => {
+    //         return {
+    //           url: `${site.siteMetadata.siteUrl}${node.path}`,
+    //           changefreq: `daily`,
+    //           priority: 0.7,
+    //         };
+    //       }),
+    //   },
+    // },
     // throwing error for some reason
     {
       resolve: `gatsby-plugin-feed`,
