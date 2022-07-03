b4c91de41bfb0dedbeb505995ff871edd6e284a1
joelmturner <joel@joelmturner.com>
2022-07-02 22:12:54 -0700

    add sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://joelmturner.com',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+  // ...other options
+};
+
+export default config;
