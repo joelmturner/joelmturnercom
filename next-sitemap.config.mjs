7c502060257a0a4c78776d7861624c6e3c792f47
joelmturner <joel@joelmturner.com>
2022-07-08 07:52:13 -0700

    fix sitemap generation

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -3,6 +3,7 @@ const config = {
   siteUrl: 'https://joelmturner.com',
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
+  exclude: ['/404', '/privacy-policy'],
   // ...other options
 };
 
