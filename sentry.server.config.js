2dc4ab19d1ceda30d7f433013d424353ccbebc8d
joelmturner <joel@joelmturner.com>
2022-07-22 21:22:52 -0700

    upgrades next and hopefully unblocks sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,6 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: '***REMOVED***',
+  tunnel: '/unblocksentry',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
