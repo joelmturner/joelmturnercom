fd412fd18ee2d9a411e1b7250893b5b8b1f7372f
joelmturner <joel@joelmturner.com>
2024-01-12 20:37:19 -0800

    use the cld-video-player api

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function getAdjustedImageSize(size: number) {
   return Math.ceil(size * 1.14);
 }
+
+export function sanitizeId(id: string) {
+    return id.replace(/[!"#$%&'()\*\+,\./:;<=>\?@\[\\\]^`{\|}~ ]+/g, '-');
+}
