ce39f9c1f981a07cd74907066641174f93d1d80f
Joel Turner <joel@joelmturner.com>
2024-10-05 00:04:09 -0700

    remove svelte gallery, replace with astro

--- src/components/svelte/galleryStore.ts
+++
@@ -1,6 +0,0 @@
-import type { IllustrationCollectionParam } from "@lib/types";
-import { writable } from "svelte/store";
-
-export const galleryStore = writable<IllustrationCollectionParam>(
-  "joelmturner_featured",
-);
--- src/lib/illustrations.ts
+++ src/lib/illustrations.ts
@@ -1,44 +1,22 @@
-import { type IllustrationItem, type Illustrations } from "./types";
-import { cloudinary } from "./cloudinary";
+import { type IllustrationItem } from "./types";
 import { getCollection } from "astro:content";
+import { getCldImageUrl } from "astro-cloudinary/helpers";
 
-export async function getIllustrations(): Promise<Illustrations> {
-  const initialImageResults: Illustrations = {
-    handletteredabcs_2016: [],
-    inktober2017: [],
-    inktober2018: [],
-    inktober2019: [],
-    inktober2021: [],
-    inktober2022: [],
-    inktober2023: [],
-    joelmturner_abcs2017: [],
-    joelmturner_featured: [],
-    jmt_dorbs: [],
-    letterclash: [],
-  };
-
+export async function getIllustrations(): Promise<IllustrationItem[]> {
   const illustrations = await getCollection("illustration");
 
-  return illustrations.reduce((imageResults, illustration) => {
-    const imageUrl = cloudinary.url(illustration.id, {
-      secure: true,
-      quality: "auto",
-      fetch_format: "auto",
-    });
-
-    const image: IllustrationItem = {
-      id: illustration.id,
-      url: imageUrl,
-      tags: illustration.data.tags || [],
-      width: illustration.data.width,
-      height: illustration.data.height,
-    };
-
-    illustration.data.tags?.forEach((tag) => {
-      const key = tag as keyof Illustrations;
-      imageResults[key] = [...(imageResults[key] || []), image];
-    });
-
-    return imageResults;
-  }, initialImageResults);
+  return illustrations
+    .map(({ id, data }) => ({
+      id,
+      url: getCldImageUrl({
+        src: id,
+        width: data.width,
+        height: data.height,
+        format: "auto",
+      }),
+      tags: data.tags || [],
+      width: data.width,
+      height: data.height,
+    }))
+    .sort((a, b) => a.id.localeCompare(b.id));
 }
